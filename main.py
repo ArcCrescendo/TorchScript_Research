cfcd942c24bcf3275e4ea793360b094aeec62a59
ArcCrescendo <ArcCrescendo@outlook.com>
2025-07-05 00:05:10 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# 这是Crescendo对于TorchScript的学习与研究。
+# 写在2005年 Sat Jul 5 00:03
+
+# 按装订区域中的绿色按钮以运行脚本。
+if __name__ == '__main__':
+    print('Welcome to TorchScript')
+
+# 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
